693bcab65cfe0a72f4e6f9bdbca8fa39de3e08d5
lucapu88 <caputoluca88@gmail.com>
2019-12-11 15:32:47 +0100

    impostato qualcosina, sapevo che non funzionava ma faccio push per salvare i dati

--- main.js
+++ main.js
@@ -3,12 +3,34 @@
 // Il numero ottenuto appare al centro del quadrato
 
 $(document).ready(function() {
-//creo un ciclo for per generare 6 contenitori
-  for (var i = 0; i < 6; i++) {
+  for (var i = 0; i < 6; i++) { //creo un ciclo for per generare 6 contenitori di quadrati
       $('body').append('<div class="quadrati-container"></div>');
   };
-  //BONUS: creo un ciclo for per creare 6 quadrati all'interno degli 8 contenitori
-  for (var i = 0; i < 6; i++) {
+  for (var i = 0; i < 6; i++) { //creo un altro ciclo for per creare 6 quadrati all'interno dei 6 contenitori
       $('.quadrati-container').append('<div class="quadrato"></div>');
   };
+  $('.quadrato').click(function(){ //dopo aver creato la griglia 6x6, al click sul singolo quadrato parte una richiesta ad ajax
+    $.ajax(
+      {
+        url : "https://flynn.boolean.careers/exercises/api/random/int", // indirizzo del server che chiamo dove viene generato un numero casuale da 0 a 9
+        method : "GET", //indico come metodo GET per chiedere dati all'url
+        success : function (data,stato) {
+          if (response <= 5) { //se response dell'url è minore/uguale a 5
+            $('.quadrato').addClass('.yellow.active'); //aggiungo la classe active per colorare il quadrato di giallo
+          } else { //se response dell'url è maggiore di 5
+            $('.quadrato').addClass('.green.active'); //aggiungo la classe active per colorarlo di verde
+          }
+        },
+        error : function (richiesta,stato,errori) {
+
+        }
+      });
+  })
+
 });
+
+
+//funzione per generare numeri random
+function generaRandom(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+};
